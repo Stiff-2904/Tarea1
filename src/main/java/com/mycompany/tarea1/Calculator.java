fe7dc73eb07d787876f0b232df283cbcf262a339
Stiff-2904 <albertstiff102030@gmail.com>
2024-03-06 15:13:05 -0600

    Prueba

--- src/main/java/com/mycompany/tarea1/Calculator.java
+++ src/main/java/com/mycompany/tarea1/Calculator.java
@@ -17,20 +17,52 @@ public class Calculator {
     }
     
     public static float divid(int num1, int num2){
-        return num1 / num2;
+        
+        if(num2 != 0) {
+            return (float)num1 / num2;
+        } else {
+            JOptionPane.showMessageDialog(null, "No se puede dividir entre 0");
+            return Float.NaN;
+        }
+        
     }
     
-    public static void usserInfo(){
-        System.out.println("Bienvenido a la CALCULATOR");
-        System.out.println("Digite el primer numero para suma");
-        System.out.println("Digite el segundo numero para resta");
-        System.out.println("Digite el tercer numero para multiplicacion");
-        System.out.println("Digite el cuarto numero para divicion");
+    public static void userInfo(){
+         JOptionPane.showMessageDialog(null, "Bienvenido a la CALCULATOR\nBy Stiff");
+         
+        int option, number1, number2;
         
-        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite el numero 1"));
+        do {
+            JOptionPane.showMessageDialog(null, "1. Sumar.\n2. Restar.\n3. Multiplicar.\n4. Dividir.\n0. Salida del programa.");
+        
+            option = Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opción"));
+            
+            if (option != 0) {
+                number1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número 1"));
+                number2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número 2"));
+                
+                switch(option) {
+                    case 1:
+                        JOptionPane.showMessageDialog(null, "El resultado es: " + sum(number1, number2));
+                        break;
+                    case 2:
+                        JOptionPane.showMessageDialog(null, "El resultado es: " + rest(number1, number2));
+                        break;
+                    case 3:
+                        JOptionPane.showMessageDialog(null, "El resultado es: " + multi(number1, number2));
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, "El resultado es: " + divid(number1, number2));
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Opción no válida");
+                }
+            }
+            
+        } while (option != 0);
     }
     
     public static void main(String[] args) {
-        usserInfo();
+        userInfo();
     }
 }
